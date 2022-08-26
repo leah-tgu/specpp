e2309d75dca0889fccc18df8eccd8e5b72ac4808
Leah Tacke genannt Unterberg <leah.tgu@pads.rwth-aachen.de>
2022-08-26 14:48:59 +0200

    playground

--- eSTMiner/src/org/processmining/estminer/specpp/util/PublicPaths.java
+++ eSTMiner/src/org/processmining/estminer/specpp/util/PublicPaths.java
@@ -2,10 +2,6 @@ package org.processmining.estminer.specpp.util;
 
 public class PublicPaths {
 
-    public static final String BASE_PATH = "estMiner\\";
-    public static final String INPUT_PATH = BASE_PATH + "input\\";
-    public static final String OUTPUT_PATH = BASE_PATH + "output\\";
-
     public static final String SAMPLE_EVENTLOG_1 = PathTools.getRelativeFolderPath(PathTools.FolderStructure.EVENT_LOG_FOLDER) + "sample_eventlog_1.xes";
     public static final String SAMPLE_EVENTLOG_2 = PathTools.getRelativeFolderPath(PathTools.FolderStructure.EVENT_LOG_FOLDER) + "sample_eventlog_2.xes";
 }
